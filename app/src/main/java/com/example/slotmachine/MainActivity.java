59fe56c600a7f0660fbdd5831c7b7fcce5f989ac
ValiantMcKee <vpmckee@widener.edu>
2020-05-25 20:24:21 -0400

    new

--- app/src/main/java/com/example/slotmachine/MainActivity.java
+++ app/src/main/java/com/example/slotmachine/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -16,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
     public Drawable apple;
     public Drawable orange;
     public ImageView[] imageViews;
-    //public UpdateCount update;
+    public ImageView firstSlot;
+    public ImageView secondSlot;
+    public ImageView thirdSlot;
+    public UpdateFruit update;
+    public int count;
+    public Handler handler;
+    public boolean on;
 
 
     @Override
@@ -24,24 +32,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firstSlot = findViewById(R.id.firstSlot);
+        secondSlot = findViewById(R.id.secondSlot);
+        thirdSlot = findViewById(R.id.thirdSlot);
+
         line = findViewById(R.id.linearLayout);
+
         cherry = getDrawable(R.drawable.cherry);
         apple = getDrawable(R.drawable.unnamed);
         orange = getDrawable(R.drawable.orange);
+
         imageViews = new ImageView[3];
 
+        handler = new Handler();
+        update = new UpdateFruit();
 
+        on = false;
 
-        for(int i =0;i<3;i++){
-            imageViews[i] = (ImageView) getLayoutInflater().inflate(R.layout.fruit_view,  null);
-            imageViews[i].setMinimumHeight(270);
-            imageViews[i].setMinimumWidth(270);
-
-            line.addView(imageViews[i]);
-        }
-        imageViews[0].setImageDrawable(cherry);
-        imageViews[1].setImageDrawable(orange);
-        imageViews[2].setImageDrawable(apple);
+        count = 0;
 
     }
+
+    public void buttonPressed(View v){
+
+        if (on) {
+            on = false;
+            handler.removeCallbacks(update);
+        } else {
+            on = true;
+            handler.postDelayed(update, 1000);
+        }
+
+    }
+
+        private class UpdateFruit implements Runnable {
+
+            public void run() {
+                count = (count%3);
+                if (count == 0) {
+                    firstSlot.setImageDrawable(apple);
+                } else if (count == 1) {
+                    firstSlot.setImageDrawable(cherry);
+                } else if (count == 2) {
+                    firstSlot.setImageDrawable(orange);
+
+                }
+                handler.postDelayed(update, 1000);
+
+                count++;
+            }
+        }
+
+
+
+
 }
